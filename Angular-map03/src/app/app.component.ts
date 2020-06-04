15bb4bad88df379a8f37a13986eb604ef88ac55e
Davide000 <bacco.davide@iisgalvanimi.edu.it>
2020-06-04 14:57:11 +0000

    done

--- Angular-map03/src/app/app.component.ts
+++ Angular-map03/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { GeoFeatureCollection } from './models/geojson.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Marker } from './models/marker.model';
+ import { Ci_vettore } from './models/ci_vettore.model';
 
 
 
@@ -22,6 +23,8 @@ export class AppComponent implements OnInit {
   obsGeoData: Observable<GeoFeatureCollection>;
   lng: number = 9.205331366401035;
   lat: number = 45.45227445505016;
+  obsCiVett : Observable<Ci_vettore[]>; //Crea un observable per ricevere i vettori energetici
+  markers : Marker[] //Marker va importato
 
   constructor(public http: HttpClient) {
   }
@@ -30,10 +33,23 @@ export class AppComponent implements OnInit {
     this.geoJsonObject = data
     console.log(this.geoJsonObject)
   }
+  //Metodo che riceve i dati e li aggiunge ai marker
+  prepareCiVettData = (data: Ci_vettore[]) =>
+  {
+    console.log(data); //Verifica di ricevere i vettori energetici
+    this.markers = []; //NB: markers va dichiarata tra le proprietà markers : Marker[]
+    for (const iterator of data) { //Per ogni oggetto del vettore crea un Marker
+     let m = new Marker(iterator.WGS84_X,iterator.WGS84_Y,iterator.CI_VETTORE);
+      this.markers.push(m);
+    }
+ }
 
   ngOnInit() {
     this.obsGeoData = this.http.get<GeoFeatureCollection>("https://3000-e45281bb-b2dd-4c17-9cf1-ed00337f766a.ws-eu01.gitpod.io/");
     this.obsGeoData.subscribe(this.prepareData);
+    this.obsCiVett = this.http.get<Ci_vettore[]>("https://3000-e45281bb-b2dd-4c17-9cf1-ed00337f766a.ws-eu01.gitpod.io/ci_vettore/90");
+    this.obsCiVett.subscribe(this.prepareCiVettData);
+
   }
   styleFunc = (feature) => {
     return ({
--- Angular-map03/src/app/models/marker.model.ts
+++ Angular-map03/src/app/models/marker.model.ts
@@ -1,7 +1,21 @@
 
+
 export class Marker {
-    constructor(
-        public lat: number,
-        public lng: number,
-        public label?: string) { }
+  icon = {}
+
+  //Quando creo un nuovo marker e verifico quale label viene passata al costruttore,
+  // se contiene il testo “Gas naturale” o “Energia elettrica” (abbreviati in Gas e Elettrica)
+  // imposto l’icona e cancello l’etichetta
+
+
+  constructor(public lat: number, public lng: number, public label?: string) {
+	if (this.label.includes("Gas")) {
+  	this.icon = { url: './assets/img/gas.ico' };   // icona Gas
+  	this.label = "";
+	}
+	if (this.label.includes("elettrica")) {
+  	this.icon = { url: './assets/img/electricity.ico' };   // icona Elettricità
+  	this.label = "";
+	}
+  }
 }
